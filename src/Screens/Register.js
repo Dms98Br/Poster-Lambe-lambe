2c5bb98ecf8db1c979f305b9ce8b61992f0aac52
Dms98Br <54338660+Dms98Br@users.noreply.github.com>
2020-09-20 23:15:11 -0300

    Screen register

---
+++ src/Screens/Register.js
@@ -0,0 +1,62 @@
+import React,{ Component } from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity as TO,
+    TextInput
+} from 'react-native'
+
+class Register extends Component{
+    state={
+        name: '',
+        email: '',
+        password: ''
+    }
+    render(){
+        return(
+            <View style={styles.container}>
+            <TextInput placeholder='Nome' style={styles.input}
+                autoFocus={true} value={this.state.name}
+                onChangeText={ name => this.setState({ name })}/>
+            <TextInput placeholder='E-mail' style={styles.input}
+                value={this.state.email}
+                keyboardType='email-address'
+                onChangeText={ email => this.setState({ email })}/>
+            <TextInput placeholder='Senha' style={styles.input}
+                value={this.state.password} secureTextEntry={true}
+                onChangeText={ password => this.setState({ password })}/>            
+            <TO onPress={() => {}} style={styles.buttom}>
+                <Text style={styles.buttomText}>Salvar</Text>
+            </TO>
+        </View>
+        )
+    }
+
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttom:{
+        marginTop: 30,
+        padding: 10,
+        backgroundColor: '#4286f4'
+    },
+    buttomText:{
+        fontSize: 20,
+        color:'#FFF'
+    },
+    input:{
+        marginTop: 20,
+        width: '90%',
+        backgroundColor: '#EEE',
+        borderWidth: 1,
+        borderColor: '#333',
+        paddingLeft: 15
+    }
+})
+export default Register
