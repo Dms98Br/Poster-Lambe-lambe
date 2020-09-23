8e4a11b2949e7b7e14bf1973c95c753f70663d4e
Dms98Br <54338660+Dms98Br@users.noreply.github.com>
2020-09-22 21:50:11 -0300

    Utilizando redux

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-/**
- * @format
- */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import Feed from './src/Screens/Feed'
 import Navigator from './src/Navigator'
+import React from 'react'
+import { Provider } from 'react-redux'
+import storeConfig from './src/store/storeConfig'
 import {name as appName} from './app.json';
+const store = storeConfig()
+const Redux = () =>(
+    <Provider store={store}>
+        <Navigator />
+    </Provider>
+)
 
-AppRegistry.registerComponent(appName, () => Navigator);
+
+AppRegistry.registerComponent(appName, () => Redux);
