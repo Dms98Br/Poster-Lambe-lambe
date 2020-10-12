82dd3e317b9d7ac8437469311a404c04d215ec36
Dms98Br <54338660+Dms98Br@users.noreply.github.com>
2020-10-11 23:10:30 -0300

    Update storeConfig.js

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -7,10 +7,12 @@ import {
 import thunk from 'redux-thunk'
 import userReducer from './reducers/user-reducers'
 import postReducer from './reducers/post-reducers'
+import messageReducer from './reducers/message-reducer'
 
 const reducers = combineReducers({
     user: userReducer,
     posts: postReducer,
+    message: messageReducer
 })
 
 const storeConfig = () => {
