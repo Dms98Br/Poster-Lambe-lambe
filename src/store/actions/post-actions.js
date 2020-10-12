5306d3dd562ce0e97e09e86957c84602926de49d
Dms98Br <54338660+Dms98Br@users.noreply.github.com>
2020-10-12 16:51:40 -0300

    Update post-actions.js

--- src/store/actions/post-actions.js
+++ src/store/actions/post-actions.js
@@ -7,7 +7,7 @@ import { setMessage } from './message-actions'
 import axios from 'axios'
 
 export const addPost = post => {
-    return dispatch => { 
+    return (dispatch, getState) => { 
         dispatch(creatingPost())              
         axios({
             url: 'uploadImage',
@@ -18,16 +18,16 @@ export const addPost = post => {
             }
         }).catch( err => {
             dispatch(setMessage({
-                title: 'Erro',
+                title: 'Erro 1',
                 text: err
             }))
         })
         .then( resp => {
             post.image = resp.data.imageUrl            
-            axios.post('/posts.json', { ...post })
+            axios.post(`/posts.json?auth=${getState().user.token}`, { ...post })
             .catch(err =>{
                 dispatch(setMessage({
-                    title: 'Erro',
+                    title: 'Erro 2',
                     text: err
                 }))
             })
@@ -40,14 +40,13 @@ export const addPost = post => {
 }
 
 export const addComment = payload =>{        
-    console.log('payload', payload);
-    return dispatch =>{
+    return (dispatch, getState) =>{
         axios.get(`/posts/${payload.postId}.json`)
         .catch( err => console.log(err))
         .then(res => {
             const comments = res.data.comments || []
             comments.push(payload.comments)
-            axios.patch(`posts/${payload.postId}.json`, {comments})
+            axios.patch(`posts/${payload.postId}.json?auth=${getState().user.token}`, {comments})
             .catch( err => console.log(err))
             .then( res => {
                 dispatch(fetchPosts())
