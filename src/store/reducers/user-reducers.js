5a99e2eaa4318180dbef07b37dc840d7ae1f7419
Dms98Br <54338660+Dms98Br@users.noreply.github.com>
2020-10-11 23:10:32 -0300

    Update user-reducers.js

--- src/store/reducers/user-reducers.js
+++ src/store/reducers/user-reducers.js
@@ -1,8 +1,14 @@
-import { USER_LOGGED_IN, USER_LOGGED_OUT } from '../actions/actionTypes-actions'
+import { 
+    USER_LOGGED_IN,
+    USER_LOGGED_OUT,
+    LOADING_USER,
+    USER_LOADED
+} from '../actions/actionTypes-actions'
 
 const initialState = {
     name: null,
-    email: null
+    email: null,
+    isLoading: false
 }
 
 const reducer = ( state = initialState, action)=>{
@@ -19,6 +25,16 @@ const reducer = ( state = initialState, action)=>{
                 ...state,
                 name: null,
                 email: null
+            }
+        case LOADING_USER:
+            return{
+                ...state,
+                isLoading: true
+            }
+        case USER_LOADED:
+            return{
+                ...state,
+                isLoading: false
             }
         default:
             return state
